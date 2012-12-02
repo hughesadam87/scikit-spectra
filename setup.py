988de8951d4266ef94e4ab0578ad0a518d719070
Adam <hugadams@glue.(none)>
2012-12-02 02:23:36 -0500

    setup.py update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 ### For now, most of these are in here for testing.  Dependency version requirements can probably be relaxed, especially chaco.
 setup(
     name='PyUvVis',
-    version='0.1.0',
+    version='0.1.1',
     author='Adam Hughes, Zhaowen Liu and GWU Physics Reeves Lab students',
     maintainer='Adam Hughes',
     maintainer_email='hughesadam87@gmail.com',
