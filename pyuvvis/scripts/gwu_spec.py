a59b1d83c0abebac1a443f7343a3710a1f05235a
Adam <hugadams@glue.(none)>
2012-12-02 03:01:30 -0500

    finally got all typos out of install for 0.1.1c

--- pyuvvis/scripts/gwu_spec.py
+++ pyuvvis/scripts/gwu_spec.py
@@ -29,11 +29,10 @@ from pyuvvis.pandas_utils.df_attrhandler import transfer_attr
 from pyuvvis.core.spec_labeltools import datetime_convert, spectral_convert, spec_slice
 from pyuvvis.core.spec_utilities import boxcar, wavelength_slices, divby
 from pyuvvis.core.baseline import dynamic_baseline
+from pyuvvis.core.imk_utils import make_root_dir, get_files_in_dir, get_shortname
 from pyuvvis.pyplots.basic_plots import specplot, timeplot, absplot, range_timeplot
-from pyplots.plot_utils import _df_colormapper, cmget
+from pyuvvis.pyplots.plot_utils import _df_colormapper, cmget
 from pyuvvis.IO.gwu_interfaces import from_timefile_datafile, get_files_in_dir, from_spec_files
-from pyuvvis.core.baseline import dynamic_baseline
-from pyuvvis.imk_utils import make_root_dir, get_files_in_dir, get_shortname
 from pyuvvis.corr2d import corr_analysis, make_ref, sync_3d, async_3d
 
 
