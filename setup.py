242c059935c3020b2b518f288ad00bc81595fe36
hugadams <hugadams@gwmail.gwu.edu>
2013-08-27 12:44:02 -0400

    fixed up setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 
-### For now, most of these are in here for testing.  Dependency version requirements can probably be relaxed, especially chaco.
+# For now, most of these are in here for testing.  Dependency version 
+#requirements can probably be relaxed, especially chaco.
 setup(
     name='PyUvVis',
     version='0.1.2-1',
@@ -14,40 +15,41 @@ setup(
                 'pyuvvis.core',
                 'pyuvvis.deprecates_oldprograms',
                 'pyuvvis.IO',
-		'pyuvvis.nptools',
+   		        'pyuvvis.nptools',
                 'pyuvvis.pandas_utils',
                 'pyuvvis.pyplots',
                 'pyuvvis.tests',
                 ],
 
-      ### See line in manifest.in that had to be included as well as this.
-      ### Can load package data using import pkgutil
-      ### data= pkgutil.get_data('pyuvvis', 'data/example_data/specdata.pickle')
-      ### Apparently this is useful for impoting using different OS distributions
+      # See line in manifest.in that had to be included as well as this.
+      # Can load package data using import pkgutil
+      # data= pkgutil.get_data('pyuvvis', 'data/example_data/specdata.pickle')
+      # Apparently this is useful for impoting using different OS distributions
       package_data={'pyuvvis' : ['data/example_data/*', 
 					### Do I want to include this?
-				      'data/gwuspecdata/*.txt',
-				      'data/gwuspecdata/fiber1/*',
+		            'data/gwuspecdata/*.txt',
+				    'data/gwuspecdata/fiber1/*',
 				    'data/gwuspecdata/NPConcentration/*',   
-				'scripts/scripttest/* '                                ]
+			 	    'scripts/scripttest/* '                                ]
                    },
     url='http://pypi.python.org/pypi/PyUvVis/',
     download_url='https://github.com/hugadams/pyuvvis',
     license='LICENSE.txt',
-    description='Pandas-based toolkit for spectral data analysis, primarily UVVis spectra for now.',
+    description='Pandas-based toolkit for spectral data analysis, primarily '
+    'UVVis spectra for now.',
     long_description=open('README.rst').read(),
     install_requires=[
-        "pandas",
-#	"numpy >= 1.6.0",  #Pands puts this in
-        "chaco",
-	"scipy", #Probably too high version requirement
-    ],
+        "pandas >= 12.0",
+#   	"numpy >= 1.6.0",  #Pandas puts this in
+#        "chaco",
+     	"scipy", #Probably too high version requirement
+                     ],
       classifiers=[
           'Development Status :: 2 - Pre-Alpha',
           'Environment :: Console',
           'Intended Audience :: Science/Research',
           'Intended Audience :: Developers',
-	  'Natural Language :: English',
+          'Natural Language :: English',
 #          'Operating System :: OSIndependent',  #This is limited only by 
           'Programming Language :: Python :: 2',
           'Topic :: Scientific/Engineering :: Visualization',
