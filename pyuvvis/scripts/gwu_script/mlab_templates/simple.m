0f2e81accd1d7d9d1e07b5e9f3d0f5d78fe94a34
hugadams <hugadams@gwmail.gwu.edu>
2013-09-27 15:42:49 -0400

    fixed matlab script

--- pyuvvis/scripts/gwu_script/mlab_templates/simple.m
+++ pyuvvis/scripts/gwu_script/mlab_templates/simple.m
@@ -13,8 +13,8 @@ basenames = {
 for i=1:length(files)
     file = files{i};            
     myData.(basenames{i}) = importdata(file);
-    myData.(basenames{i}).labels = myData.(files{i}).data(:,1);
-    myData.(basenames{i}).data = myData.(files{i}).data(:,2:end);
+    myData.(basenames{i}).labels = myData.(basenames{i}).data(:,1);
+    myData.(basenames{i}).data = myData.(basenames{i}).data(:,2:end);
 end
 
 %(attrnames)s
