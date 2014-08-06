b88cde716235de6c53d5d07b09ed15ac25bcdd77
Your Name <you@example.com>
2014-08-06 19:35:39 -0400

    Checking in prototypes (non overwriting) of new indexing system

---
+++ pyuvvis/core/test_index.py
@@ -0,0 +1,135 @@
+from pandas import Float64Index
+import numpy as np
+from units import SPECUNITS, UnitError
+
+def _parse_unit(unit, unitdict):
+   """ Given a string unit (ie nm), returns the corresponding unit
+   class."""
+   if unit not in unitdict:
+      raise UnitError('Invalid unit "%s".  Choose from %s' % 
+                      (unit, sorted(unitdict.keys()) ) )
+   return unitdict[unit]  
+
+class ConversionIndex(Float64Index):
+   """ Base class for pyuvvis.  To overwrite, requires:
+        - replace unitdict with a dictionary from units.py, eg SPECUNITS
+        - overwrite convert(), which defines conversions on the unit types.
+   Since we are subclassing a numpy array and not a basic python object, 
+   we don't write __init__(); rather, __new__ and __array_finalize__ are
+   employed (http://docs.scipy.org/doc/numpy/user/basics.subclassing.html)
+   """
+
+   unitdict = None 
+
+   def __new__(cls, input_array, unit=None):
+      """ Unit is valid key of unitdict """
+      obj = np.asarray(input_array).view(cls)
+      obj.unit = _parse_unit(unit, cls.unitdict)
+      return obj
+
+   # I am not really worred about other constructors yet...
+   def __array_finalize__(self, obj):
+      """No matter what constructor called, this will get called, so does
+      all the housekeeping."""
+      if obj is None: 
+         return
+      
+      # Can be called form slicing or other reasons, and I don't always know
+      # if unit is still a string/None, or if it's already a Unit class.
+      # Thus, I let both cases work, even though don't know when is what
+      self.unit = getattr(obj, 'unit', None)
+
+##      if isinstance(unit, str) or unit is None:
+##         self.unit = _parse_unit(unit, self.unitdict)
+##      else:
+          #self.unit = unit
+
+   def convert(self, outunit):
+      """Convert spectral values based on string outunit."""
+      outunit = _parse_unit(outunit, self.unitdict)
+      inunit = self.unit
+
+      # Unit not changed, or set to None
+      if outunit == self.unit or not outunit.short:
+         return self.__class__(self, unit=inunit)
+
+      # If current unit is None, just set new unit
+      if not self.unit.short:
+         return self.__class__(self, unit = outunit.short)
+
+      # Convert non-null unit to another non-null unit   
+      else:
+         
+         # If both untits are proportional
+         if inunit.proportional and outunit.proportional:
+            arrayout = self * (inunit.mapping / outunit.mapping)
+         
+         elif inunit.proportional and outunit.reciprocal:
+            arrayout =  1.0 / (self * (inunit.mapping / outunit.mapping))
+                        
+         elif inunit.reciprocal and outunit.proportional:
+            arrayout = 1.0 / (self * (outunit.mapping/inunit.mapping))
+            
+         elif inunit.reciprocal and outunit.reciprocal:
+            arrayout = self * (outunit.mapping/inunit.mapping)
+
+            
+         # Should only occur if user sets proportional or a weird value
+         else:
+            raise UnitError("Something wrong; could not determine mapping"
+                            "logic...")
+
+         return self.__class__(arrayout, unit = outunit.short)         
+         
+
+   def _parse_unit(self, unit):
+      """ Given a string unit (ie nm), returns the corresponding unit
+      class."""
+      if unit not in self.unitdict:
+         raise UnitError('Invalid unit "%s".  Choose from %s' % 
+                         (unit, sorted(self.unitdict.keys()) ) )
+      return self.unitdict[unit]      
+
+
+   #Email list about this distinction
+   def __repr__(self):
+      """ Used internally in code, like if I print self fromin function."""
+      out = super(ConversionIndex, self).__repr__()        
+      return out.replace(self.__class__.__name__, '%s[%s]' %
+                         (self.__class__.__name__, self.unit.short))
+
+   def __unicode__(self):
+      """ Returned on printout call.  Not sure why repr not called... """
+      out = super(ConversionIndex, self).__unicode__()        
+      return out.replace(self.__class__.__name__, '%s[%s]' % 
+                         (self.__class__.__name__, self.unit.short))
+
+   # PROMOTED UNIT METHODS
+   def __getattr__(self, attr):
+      """ Defer attribute call to self.unit"""
+      try:
+         return getattr(self.unit, attr)
+      except AttributeError:
+         raise AttributeError('%s has no attribute "%s".' % 
+                              (self.__class__.__name__, attr) )
+
+
+class SpecIndex(ConversionIndex):
+   """ """
+   unitdict = SPECUNITS         
+
+
+if __name__ == '__main__':
+   x = SpecIndex(np.linspace(0,50), unit='nm')
+#   y = Float64Index(np.linspace(0,50))
+#   print y*70
+   #print x**2
+   #print x.unit
+   #y = x**2
+   #print y.unit
+#   print np.asarray(x)
+#   print x*80
+
+#   print x
+#   print x.categor
+#   _unit_valid('nm')
---
+++ pyuvvis/core/units.py
@@ -0,0 +1,181 @@
+from math import pi
+
+""" Unit Class: series of metadata for various index units used throughout.
+All class attributes to conserve memory.  Unit conversions are defined
+in the respective index classes calling these"""
+
+### Change spectral labels ###
+h = 6.626068*10**-34          #Planck's constant m**2 kg / s
+eVtoJ = 1.60217646 * 10**-19  #Number of Joules in one eV (needed becasue eV is not the standard MKS unit of energy)
+c = 299792458.0               #speed of light m/s
+
+class UnitError(Exception):
+   """ """
+
+
+# Consider adding a better __repr__ to these for when objects are called?
+class Unit(object):
+   """ """
+   short = ''
+   full = ''
+   symbol = ''
+   mapping = 1.0
+   proportional = True
+   
+   @property
+   def reciprocal(self):
+      if self.proportional:
+         return False
+      else:
+         return True
+   
+   
+class TempUnit(Unit):
+   """ Temperature units.  ALl conversions go through Kelvin. """
+
+class Kelvin(TempUnit):
+   short = 'K'
+   full = 'Kelvin' #Proper names, keep this way?
+   symbol = 'r$^{\deg}K$' #Isn't degree Kelvin technially wrong?
+   mapping = 1.0
+
+class Celsius(TempUnit):
+   short = 'C'
+   full = 'Celsius'
+   symbol = 'r$^{\deg}C$'
+#   mapping = #273 - ??? How
+
+# SPECTRAL UNITS   
+class SpecUnit(Unit):
+   """  Spectroscopy units (nm, m, ev etc...)
+   Allow for recpricol units such as cm-1; used by converters.  All 
+   conversions go through unit, considered the core unit.  Adds category
+   to distinguish between wavelength, wavenumber, energy, frequency."""
+
+   category = ''
+   
+   @property
+   def symbol(self):
+      if self.category == 'wavelength':
+         return r'$\lambda$'
+   
+      elif self.category == 'wavenumber':
+         return r'$\kappa$'
+      
+      elif self.category == 'frequency':
+         return r'$\nu$'
+   
+      elif self.category == 'energy':
+         return r'$\epsilon$'
+      
+      elif not self.category: #NullUnit
+         return ''
+      
+      else:
+         raise UnitError('invalid category' % self.category)
+   
+class NullSpecUnit(SpecUnit):
+   """Used by conversions when spectral unit not specified.  Inherits
+   from Unit may cause subclassing problems.  Subclasses  inspection should
+   consider NullUnit like "if isnstance(Parent) or isinstance(NullUnit)"""
+   short = None
+   full = 'No Spectral Unit'
+   mapping = 1.0   
+   category = ''
+   proportional = True
+   
+class Meters(SpecUnit):
+   short = 'm'
+   full = 'meters'
+   category = 'wavelength'
+   proportional = True
+   mapping = 1.0
+
+class Centimeters(SpecUnit):
+   short = 'cm'
+   full = 'centimeters'
+   category = 'wavelength'
+   proportional = True
+   mapping = .01
+   
+class Micrometers(SpecUnit):
+   short = 'um'
+   full = 'microns'
+   category = 'wavelength'
+   proportional = True
+   mapping = .000001   
+   
+class Nanometers(SpecUnit):
+   short = 'nm'
+   full = 'nanometers'
+   category = 'wavelength'
+   proportional = True
+   mapping = .000000001
+   
+class Metersinverse(SpecUnit):
+   """ Cycles per distance/wavenumber"""
+   short = 'k'
+   full = 'inverse meters' #or cycles per meter or radians per meter
+   category = 'wavenumber'
+   proportional = False
+   mapping = 1  #Right?  not factor 2 pi.  Tested with a conversion utility
+   
+class Centimetersinverse(SpecUnit):
+   short = 'cm-1'
+   full = 'inverse centimeters'
+   category = 'wavenumber'
+   proportional = False
+   mapping = .01
+   
+class Micrometersinverse(SpecUnit):
+   short = 'um-1'
+   full = 'inverse microns'
+   category = 'wavenumber'
+   proportional = False
+   mapping = .01   
+   
+class Nanometersinverse(SpecUnit):
+   short = 'nm-1'
+   full = 'inverse nanometers'
+   category = 'wavenumber'
+   proportional = False
+   mapping = .01      
+   
+class Frequency(SpecUnit):
+   short = 'f'
+   full = 'hertz'
+   category = 'frequency'
+   proportional = False
+   mapping = c 
+   
+class Angularfrequency(SpecUnit):
+   short = 'w'
+   full = 'radians per second'
+   category = 'frequency'
+   proportional = False
+   mapping = 2.0 * pi * c
+   
+class Electronvolts(SpecUnit):
+   short = 'ev'
+   full = 'electron volts'
+   category = 'energy'
+   proportional = False
+   mapping = h*c/(eVtoJ)
+
+   
+_specunits = (
+             Meters(), 
+             Metersinverse(), 
+             Nanometers(), 
+             Centimeters(), 
+             Centimetersinverse(),
+             Micrometers(),
+             Micrometersinverse(),
+             Electronvolts(),
+             Frequency(),
+             Angularfrequency(),
+             NullSpecUnit()
+             )
+
+# Dictionary keyed by short for easy use by converters
+SPECUNITS = dict((obj.short, obj) for obj in _specunits)
