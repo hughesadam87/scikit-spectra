c4158d34b5d0f67c79121120a1971eed36c86be7
Your Name <you@example.com>
2014-08-12 10:18:05 -0400

    bam

--- pyuvvis/core/units.py
+++ pyuvvis/core/units.py
@@ -5,13 +5,12 @@ All class attributes to conserve memory.  Unit conversions are defined
 in the respective index classes calling these"""
 
 ### Change spectral labels ###
-h = 6.626068*10**-34          #Planck's constant m**2 kg / s
+H = 6.626068*10**-34          #Planck's constant m**2 kg / s
 eVtoJ = 1.60217646 * 10**-19  #Number of Joules in one eV (needed becasue eV is not the standard MKS unit of energy)
-c = 299792458.0               #speed of light m/s
+C = 299792458.0               #speed of light m/s
 
 class UnitError(Exception):
    """ """
-
 
 # Consider adding a better __repr__ to these for when objects are called?
 class Unit(object):
@@ -19,49 +18,50 @@ class Unit(object):
    short = ''
    full = ''
    symbol = ''
-   mapping = 1.0
    proportional = True
+   _canonical = False
    
-   @property
-   def reciprocal(self):
-      if self.proportional:
-         return False
-      else:
-         return True
+   def to_canonical(self):
+      NotImplemented
+      
+   def from_canonical(self):
+      NotImplemented
    
-
 # How to do temperature conversions with +/- 273   
 class TempUnit(Unit):
    """ Temperature units.  ALl conversions go through Kelvin. """
-   proportional = True
 
+#http://www.metric-conversions.org/temperature/fahrenheit-to-kelvin.htm
 class Kelvin(TempUnit):
    short = 'K'
    full = 'Kelvin' #Proper names, keep this way?
    symbol = 'r$^{\deg}K$' #Isn't degree Kelvin technially wrong?
-   mapping = 1.0
+   _canonical = True
+   
+   def to_canonical(self, x):
+      return x
+      
+   def from_canonical(self, x):
+      return x
 
 class Celsius(TempUnit):
    short = 'C'
    full = 'Celsius'
    symbol = 'r$^{\deg}C$'
-#   mapping = #273 - ??? How
 
 class SoluteUnit(Unit):
    """ Goes through molar.  Test case """
-   proportional = True 
    
 class Moles(SoluteUnit):
    short = 'M'
    full = 'moles'
    symbol = 'M'
-   mapping = 1.0
+   _canonical = True
    
 class Millimoles(SoluteUnit):
    short = 'mM'
    full = 'millimoles'
    symbol = 'mM' #Kind of redundant
-   mapping = .001
 
 # SPECTRAL UNITS   
 class SpecUnit(Unit):
@@ -98,87 +98,143 @@ class NullSpecUnit(SpecUnit):
    consider NullUnit like "if isnstance(Parent) or isinstance(NullUnit)"""
    short = None
    full = 'No Spectral Unit'
-   mapping = 1.0   
    category = ''
-   proportional = True
    
 class Meters(SpecUnit):
    short = 'm'
    full = 'meters'
    category = 'wavelength'
-   proportional = True
-   mapping = 1.0
+   _canonical = True
+   
+   @classmethod
+   def to_canonical(self, x):
+      return x
+      
+   @classmethod
+   def from_canonical(self):
+      return x
 
 class Centimeters(SpecUnit):
    short = 'cm'
    full = 'centimeters'
    category = 'wavelength'
-   proportional = True
-   mapping = .01
+
+   @classmethod   
+   def to_canonical(self, x):
+      return x / 100.0
+
+   @classmethod      
+   def from_canonical(self, x):
+      return 100.0 * x  
    
 class Micrometers(SpecUnit):
    short = 'um'
    full = 'microns'
    category = 'wavelength'
-   proportional = True
-   mapping = .000001   
+   
+   @classmethod
+   def to_canonical(self, x):
+      return x / 100000.0 
+      
+   @classmethod
+   def from_canonical(self, x):
+      return 100000.0 * x
    
 class Nanometers(SpecUnit):
    short = 'nm'
    full = 'nanometers'
    category = 'wavelength'
-   proportional = True
-   mapping = .000000001
+   
+   @classmethod
+   def to_canonical(self, x):
+      return x / 100000000.0 
+      
+   @classmethod
+   def from_canonical(self, x):
+      return 100000000.0 * x   
    
 class Metersinverse(SpecUnit):
    """ Cycles per distance/wavenumber"""
    short = 'k'
    full = 'inverse meters' #or cycles per meter or radians per meter
    category = 'wavenumber'
-   proportional = False
-   mapping = 1  #Right?  not factor 2 pi.  Tested with a conversion utility
+   
+   # TEST THIS
+   @classmethod
+   def to_canonical(self, x):
+      return 1.0 / x
+      
+   @classmethod
+   def from_canonical(self, x):
+      return 1.0 / x
    
 class Centimetersinverse(SpecUnit):
    short = 'cm-1'
    full = 'inverse centimeters'
    category = 'wavenumber'
-   proportional = False
-   mapping = .01
+
+   # DEFINE IN TERMS OF TO METERS?
+   @classmethod
+   def to_canonical(self, x):
+      return (1.0 / x) * (0.01)
+      
+   @classmethod
+   def from_canonical(self, x):
+      return 1.0 / (x * 100.0)
    
 class Micrometersinverse(SpecUnit):
    short = 'um-1'
    full = 'inverse microns'
    category = 'wavenumber'
-   proportional = False
-   mapping = .01   
    
 class Nanometersinverse(SpecUnit):
    short = 'nm-1'
    full = 'inverse nanometers'
    category = 'wavenumber'
-   proportional = False
-   mapping = .01      
-   
+        
 class Frequency(SpecUnit):
+   """ lambda = v/f where v is c for electromagnetic radiation; in general,
+   it's called the phase speed. """
    short = 'f'
    full = 'hertz'
    category = 'frequency'
-   proportional = False
-   mapping = c 
+   
+   @classmethod
+   def to_canonical(self, x):
+      meters = Meters.to_canonical(x)
+      return C / meters
+      
+   @classmethod
+   def from_canonical(self, x):
+      return C / x
    
 class Angularfrequency(SpecUnit):
    short = 'w'
    full = 'radians per second'
    category = 'frequency'
-   proportional = False
-   mapping = 2.0 * pi * c
+   
+   @classmethod
+   def to_canonical(self, x):
+      meters = Meters.to_canonical(x)
+      return (2.0 * pi * C) / meters
+      
+   @classmethod
+   def from_canonical(self, x):
+      return (2.0 * pi * C) / x
    
 class Electronvolts(SpecUnit):
    short = 'ev'
    full = 'electron volts'
    category = 'energy'
-   proportional = False
-   mapping = h*c/(eVtoJ)
+
+   @classmethod
+   def to_canonical(self, x):
+      meters = Meters.to_canonical(x)
+      return (H*C/(eVtoJ) ) / meters
+      
+   @classmethod
+   def from_canonical(self, x):
+      return (H*C/(eVtoJ)) / x
 
    
 _specunits = (
@@ -204,3 +260,9 @@ _soluteunits = (Moles(),
                 NullSpecUnit ()) #Just for testing
 
 SOLUTEUNITS = dict((obj.short, obj) for obj in _soluteunits)
+
+if __name__ == '__main__':
+   f = Frequency()
+   ev = Electronvolts()
+   print f.from_canonical(1)
+   print ev.to_canonical(1)
