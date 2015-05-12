dc126b8d28d8e4e2dc07700e5062d97f63fb4f8d
Adam <hugadams@gwmail.gwu.edu>
2015-05-12 19:32:15 -0400

    removed scripts

--- skspec/scripts/gwu_script/gwuspec.py
+++
@@ -1,9 +0,0 @@
-from gwu_controller import Controller
-
-def main(*args):
-    controller = Controller.from_namespace()
-    controller.start_run()
-    
-    
-if __name__ == '__main__':
-    main()
